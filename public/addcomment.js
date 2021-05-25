16bdae8fee30e72762f2c55038707e374640629d
Gian Zamora <gz2016@att.net>
2021-05-25 00:05:39 -0700

    added update functionality

---
+++ public/addcomment.js
@@ -0,0 +1,32 @@
+// this selects the comment button and triggers the addComment function
+document.querySelector('#comment-button').addEventListener('click', addComment);
+
+async function addComment(event) {
+    event.preventDefault();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1 
+    ];
+    // grabs content entered from comment form
+    const comment_content = document.querySelector('#comment-content').value.trim();
+
+    // if comment was entered, then it is posted and marked in json
+    if (content_content) {
+        const response = await fetch('/api/comments',  {
+            method: 'POST',
+            body: JSON.stringify({
+                comment_content,
+                post_id,
+            }),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Error posting comment...')
+        }
+    }
+}
--- public/updatepost.js
+++ public/updatepost.js
@@ -1,14 +1,45 @@
+// attaches to create/update button and calls update function
+document.querySelector('#create-post-button').addEventListener('click', createPost);
 // attaches to edit button and calls edit function
-document.querySelector('#edit-button').addEventListener('click', editPost);
+document.querySelector('#edit-post-button').addEventListener('click', editPost);
 // attaches to deleet button and calls delete function
-document.querySelector('#delete-button').addEventListener('click', deletePost);
+document.querySelector('#delete-post-button').addEventListener('click', deletePost);
+
+async function createPost(event) {
+    event.preventDefault();
+
+    const post_title = document.querySelector('#post-title').innerHTML;
+    const post_body = document.querySelector('#post-body').innerHTML;
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    
+    if (post_body) {
+        const response = await fetch(`/api/posts/${post_id}`, {
+            method: 'PUT', 
+            body: JSON.stringify({
+                post_title,
+                post_id
+            }),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace(`/dashboard`);
+        } else {
+            alert('Error submitting post...');
+        }
+    }
+};
 
 // this is meant to update the post
 async function editPost(event) {
     event.preventDefault();
 
-    const post_title = document.querySelector('post-title').innerHTML;
-    const post_body = document.querySelector('post-body').innerHTML;
+    const post_title = document.querySelector('#post-title').innerHTML;
+    const post_body = document.querySelector('#post-body').innerHTML;
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -16,7 +47,7 @@ async function editPost(event) {
     console.log(post_title, post_body);
     document.location.replace(`/edit/${post_id}`);
 
-
+    
 };
 
 async function deletePost(event) {
