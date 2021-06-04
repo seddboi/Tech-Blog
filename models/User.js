82a296965297d0c3a5ba98596a6e78fe6b6b45cd
Gian Zamora <gz2016@att.net>
2021-06-03 22:42:21 -0700

    forgot to add bcrypt module

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const {Model, Datatypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
     checkPass(pass) {
