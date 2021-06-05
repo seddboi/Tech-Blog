99ecfa3edac8df73da008f3fb4454b7281232f32
Gian Zamora <gz2016@att.net>
2021-06-04 17:04:16 -0700

    re pushed content

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const path = require('path');
 const sequelize = require('./config/connection');
 const express = require('express');
-const routes = require('./controllers');
-
+const routes = require('./controllers')
+const controllers = require('./controllers');
 const exphbs = require('express-handlebars');
 // const hbs = exphbs.create({helpers});
-
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'thelordscheeps',
+    secret: 'super secret secret',
     cookie: {},
+    resave: false,
     saveUninitialized: true,
     store: new SequelizeStore ({
         db: sequelize,
@@ -25,11 +24,9 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-
-app.use('/', controller);
+app.use(routes);
 
 sequelize.sync({ force: false }).then( () => {
     app.listen(PORT, () => console.log('Now listening...'))
